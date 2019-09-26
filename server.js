654d8dc41500283ea4c0179709e748c692fb2a35
olivamat <matiasholeh@gmail.com>
2019-09-26 20:49:41 +0300

    fix bag

--- server.js
+++ server.js
@@ -2,23 +2,19 @@ const express = require("express");
 const costs = require("./src/DB/costs/all-costs.json");
 const app = express();
 
-const PORT = process.env.PORT || 3002;
-// console.log(costs);
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 app.get("/costs", function(req, res) {
   if (req.query.category) {
-    // console.log(req.query.category);
-    // console.log(costs);
     let filtredCosts = costs.filter(cost =>
       cost.categories.some(costCat => costCat === req.query.category)
     );
     if (filtredCosts.length >= 1) {
       res.send({ status: "success", product: filtredCosts });
-    } else console.log(filtredCosts);
-    res.send({ status: "no products", product: filtredCosts });
+    } else res.send({ status: "no products", product: filtredCosts });
   } else res.send(costs);
 });
 // app.get("/costs", function(req, res) {
@@ -29,7 +25,9 @@ app.get("/costs/:id", function(req, res) {
   let oneCosts = costs.find(costs => {
     return costs.id === Number(req.params.id);
   });
-  res.status(200).send({ status: "success", product: oneCosts });
+  if (oneCosts) {
+    res.status(200).send({ status: "success", product: oneCosts });
+  } else res.send({ status: "Not found" });
 });
 
 app.use(function(req, res, next) {
